b8eb52d86e40e5a3bb8baff80c441cea3cbd75c4
Your Name <you@example.com>
2025-09-14 22:24:52 -0300

    new try

--- hashmap.c
+++ hashmap.c
@@ -94,6 +94,20 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
+    if(map == NULL || key== NULL)
+    {
+        return NULL;
+    }
+
+    long pos = hash(key, map->capacity);
+    while(map->buckets[pos] != NULL)
+    {
+        if(is_equal(map->buckets[pos]->key, key))
+        {
+            map->current = pos;
+            return map->buckets;
+        }
+    }
 
 
     return NULL;
