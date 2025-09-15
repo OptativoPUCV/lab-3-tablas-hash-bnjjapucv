9cf8b7a8ff00205c7e7784823aedebda0d6a79a2
Your Name <you@example.com>
2025-09-14 22:01:54 -0300

    new try

--- hashmap.c
+++ hashmap.c
@@ -51,7 +51,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     {
         pos = (pos+1) % map->capacity;
     }
-    map->buckets = createPair(key);
+    map->buckets = createPair(key, value);
     map->current = pos;
 
 }
